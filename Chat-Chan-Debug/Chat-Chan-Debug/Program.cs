4dd2fa63c631eed88c594bb0a3ad75be4e88b34b
Potato1682 <potato1682@users.noreply.github.com>
2020-03-30 13:21:02 +0900

    Initial commit

---
+++ Chat-Chan-Debug/Chat-Chan-Debug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chat_Chan_Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chat-Chan Debug Application\nCreated by P2PDevelop\nThis Application are managed by MIT License.\n\n");
+        }
+    }
+}
